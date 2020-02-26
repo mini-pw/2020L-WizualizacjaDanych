660233b9ce2d8187562bfc88cbeb6e7e1f34b43e
Grymula Franciszek <grymulaf@p21903w.MINI>
2020-02-26 10:52:15 +0100

    lab_26_02_2020

--- Materiały/gr1/ggplot2-intro.R
+++ Materiały/gr1/ggplot2-intro.R
@@ -1,7 +1,23 @@
 library(ggplot2)
-library(dplyr)
+library(data.table)
+library(magrittr)
 
-gd <- read.csv("./data/grants_small.csv")
+gd <- fread("./data/grants_small.csv", encoding = 'UTF-8', stringsAsFactors = FALSE)
+
+colnames(gd)
+
+gd[, budget := as.numeric(gsub(pattern = '[a-z A-Z:]', replacement = '', x = budget))]
+gd[, institution := sapply(strsplit(institution, ', '), function(x) x[1])]
+
+gd[institution %in% c('Uniwersytet Warszawski',
+                      'Politechnika Warszawska',
+                      'AGH'),
+   .(mean_budget=mean(budget)), by=institution] %>%
+  ggplot(aes(x = institution, y = mean_budget)) +
+  geom_col() +
+  coord_flip()
+
+
 
 # aktualizacja forka
 
