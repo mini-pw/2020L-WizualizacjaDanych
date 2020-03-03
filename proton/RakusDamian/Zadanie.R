1a22d90ff9a47a7a21d45f4c9da0c0db40689597
Damian <damian.rakus@gmail.com>
2020-03-03 19:39:50 +0100

    Solved proton

--- proton/RakusDamian/Zadanie.R
+++ proton/RakusDamian/Zadanie.R
@@ -15,11 +15,26 @@ for(password in top1000passwords) {
   }
 }
 
-pietraszko = subset(employees, name == 'Sławomir' & surname == "Pietraszko")
+proton(action = "login", login = john_login, password = john_password)
+
+pietraszko = subset(employees, name == 'Slawomir' & surname == "Pietraszko")
 pietraszko_login = pietraszko$login
-pietraszko_logs = subset(logs,login == pietraszko_login)
+pietraszko_logs = subset(logs, login == pietraszko_login)
 
 library(plyr)
 servers = count(pietraszko_logs, vars = 'host')
 
-top_server = servers[order(),]$freq
+# order servers 1 - most visited, n - least visited
+sorted_servers = servers[order(-servers$freq),]
+top_server = sorted_servers[1,]
+
+# login to Pietraszko's favorite server
+top_host_ip = as.character(top_server$host)
+proton(action = "server", host = top_host_ip)
+
+commands <- gsub(" .*", "", bash_history)
+command_count <- count(commands)
+reversed_command_count <- command_count[order(command_count$freq),]
+pietraszko_password = as.character(reversed_command_count[1,'x'])
+
+proton(action = 'login', login = pietraszko_login, password = pietraszko_password)
