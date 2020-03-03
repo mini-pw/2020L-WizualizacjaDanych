1cbb9ec3f2aefa0b8ee134d2d6550ec92d8ead01
Damian <damian.rakus@gmail.com>
2020-03-03 21:19:43 +0100

    commit

--- Materiały/gr1/ggplot2-intro.R
+++ Materiały/gr1/ggplot2-intro.R
@@ -1,7 +1,7 @@
 library(ggplot2)
 library(dplyr)
 
-gd <- read.csv("./data/grants_small.csv", stringsAsFactors = FALSE)
+gd <- read.csv("../../data/grants_small.csv", stringsAsFactors = FALSE)
 # edit(gd)
 # summary(gd)
 
@@ -51,5 +51,33 @@ mutate(gd,
 
 
 
-# geometrie
+# notatki moje
+toupper(budget) # error, nie ma takiej zmiennej
+toupper(budget)
+# ale to poniżej git
+mutate(gd, budget_large = toupper(budget))
+
+as.numeric(gsub(pattern = "[a-z: ]","", gd$budget, ignore.case = TRUE))
+
+mutate(gd, budget_numeric = as.numeric(gsub(pattern = "[a-z: ]","", budget, ignore.case = TRUE)) )
+
+
+
+pull(gd, institution) # wariant dla gd$institution
+
+lapply(strsplit(gd[['institution']],", "), first)
+# lapply(strsplit(gd[['institution']],", "), function(i) i[1]) - drugi sposób
+
+mutate(gd, budget_numeric = as.numeric(gsub(pattern = "[a-z: ]","", budget, ignore.case = TRUE)),
+       institution = sapply(strsplit(gd[['institution']],", "), function(i) i[1])) %>% 
+  group_by(institution) %>% 
+  summarise(mean_budget = mean(budget_numeric)) %>% 
+  filter(institution %in% c("Politechnika Warszawska",
+                            "Politechnika Łódzka",
+                            "Akademia Górniczo-Hutnicza im. St.Staszica w Krakowie")) %>% 
+  ggplot(aes(x= institution, y= mean_budget)) +
+  geom_col() +
+  coord_flip()
+
+# %>% Wpisuje do funckcji argument z lewej jako pierwszy nienazwany argument
 
