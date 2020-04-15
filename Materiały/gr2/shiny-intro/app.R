9908b88a03fef027fbbcaa66004c2cc36e1b2425
Michał Burdukiewicz <michalburdukiewicz@gmail.com>
2020-04-15 15:41:27 +0200

    added number of points

--- Materiały/gr2/shiny-intro/app.R
+++ Materiały/gr2/shiny-intro/app.R
@@ -2,15 +2,27 @@ library(shiny)
 library(ggplot2)
 
 ui <- fluidPage(title = "My first Shiny app",
-  plotOutput("point_plot")
+                selectInput("point_color", label = "Select color", 
+                            choices = c("black", "red", "#eeeeee",
+                                        "#F5F5DC", "#C2B280")),
+                sliderInput("number_of_points", "How many points?", 
+                            min = 5, max = 50, value = 10),
+                plotOutput("point_plot")
 )
+
 
 server <- function(input, output) {
   
+  point_df <- reactive({
+    data.frame(x = 1L:input[["number_of_points"]], 
+               y = rpois(input[["number_of_points"]], 1.5))
+  })
+  
+  
   output[["point_plot"]] <- renderPlot({
-    ggplot(data = data.frame(x = 1L:10, y = rpois(10, 1.5)),
+    ggplot(data = point_df(),
            aes(x = x, y = y)) +
-      geom_point()
+      geom_point(color = input[["point_color"]])
   }) 
   
 }
