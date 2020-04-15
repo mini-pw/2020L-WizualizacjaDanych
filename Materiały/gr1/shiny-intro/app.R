5a97adbe403f8d51958941ffde8ea47950a31914
Michał Burdukiewicz <michalburdukiewicz@gmail.com>
2020-04-15 09:39:04 +0200

    added app

--- Materiały/gr1/shiny-intro/app.R
+++ Materiały/gr1/shiny-intro/app.R
@@ -3,7 +3,9 @@ library(ggplot2)
 
 ui <- fluidPage(
   sidebarLayout(sidebarPanel = sidebarPanel(
-    textInput("plot_title", "Specify plot title")
+    textInput("plot_title", "Specify plot title"),
+    sliderInput("axis_x_range", "Specify range", 
+                min = 0, max = 10, value = c(0, 10))
   ),
   mainPanel = mainPanel(
     plotOutput("point_plot"),
@@ -24,7 +26,9 @@ server <- function(input, output) {
   output[["point_plot"]] <- renderPlot({
     ggplot(data = point_df(),
            aes(x = x, y = y)) +
-      geom_point()
+      geom_point() +
+      coord_cartesian(xlim = input[["axis_x_range"]]) +
+      ggtitle(input[["plot_title"]])
   }) 
   
 }
